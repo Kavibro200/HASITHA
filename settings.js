4071159888be6985764c2e31926aa76ec98a51a1
Kavibro200 <amithaanushani61@gmail.com>
2025-03-10 22:27:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+rconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'මෙතනට ඔයාගෙ සිසන් id එක දාන්න' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bzAVkIxI#oAnicPZDG9n06LWzj0fNoRsKorsmMHmTud9WDHU6_wc' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
